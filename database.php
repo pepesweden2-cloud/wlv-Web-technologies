63aee46850fa18cef7d906121d4cd8a372c41d05
Dimitrios Foukis, 2435402, Online Web technologies <pepesweden2@gmail.com>
2025-10-24 11:50:36 +0800

    Add multiple database functions for manufacturers and orders
    finaisation

--- database.php
+++ database.php
@@ -38,3 +38,126 @@ class Database {
             return 0;
         }
     }
+
+    public static function manufacturerCount($conn) {
+        try {
+            $conn->exec("set names 'utf8'");
+            $sql = "select count(*) as c from `manufacturer`";
+            $c = 0;
+            foreach ($conn->query($sql) as $row) {
+                $c = $row['c'];
+            }
+            return "{\"count\":$c}";
+        } catch (Exception $e) {
+            return $e->getMessage() . "{'count':-1}";
+        }
+    }
+    
+    public static function getAllCategories($conn) {
+        try {
+            $conn->exec("set names 'utf8'");
+            
+            $sql = "SELECT distinct `id` as `code`, `description` FROM `catagories` WHERE 1";
+
+            $records = array();
+            foreach ($conn->query($sql) as $row) {
+                array_push($records, $row);
+            }
+            return json_encode($records);
+        } catch (Exception $e) {
+            return $e->getMessage() . "{'count':-1}";
+        }
+    }
+    
+    public static function getOrders($conn) {
+        try {
+            $conn->exec("set names 'utf8'");
+            $sql = "select `orderID`,`pricedata`.`productID` as productID,`username`,`quantity`,`when`,`description`,`price` from `orders`,`pricedata` where `orders`.`productID` = `pricedata`.`productID`  order by `when` desc";
+            $records = array();
+            foreach ($conn->query($sql) as $row) {
+                array_push($records, $row);
+            }
+            return json_encode($records);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            return null;
+        }
+    }
+	
+    public static function login($conn, $username, $password){
+        try {
+            $conn->exec("set names 'utf8'");
+			$sql = "SELECT `users`.`username` as username,`password`,`email`,IFNULL(`manufacturer`.`id`,0) as gasstationID FROM `users` left join `manufacturer` on `users`.`username`=`manufacturer`.`username` WHERE `users`.`username`='$username' and `password`='$password'";
+            $stmt = $conn->query($sql);
+            $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($records as $record) {
+                return $record;
+            }
+            return null;
+        } catch (Exception $e) {
+            return null;
+        }
+        
+    }
+    
+    public static function changePrice($conn, $productid, $price){
+        try {
+            $conn->exec("set names 'utf8'");
+			$sql = "update `products` set `price`=$price where  `id` = $productid";
+            $stmt = $conn->exec($sql);
+            echo $sql;
+            return 1;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public static function getAllManufacturers($conn) {
+        try {
+            $conn->exec("set names 'utf8'");
+			$sql = "SELECT `id`, `description`, `municipalityID`, `municipalityName`, `countyID`, `countyName`, `address`, `phone1` FROM `manufacturer` WHERE 1";
+            $records = array();
+            foreach ($conn->query($sql) as $row) {
+                array_push($records, $row);
+            }
+            return json_encode($records);
+        } catch (Exception $e) {
+
+            return null;
+        }
+    }
+    
+    public static function placeOrder($conn, $username, $product, $quantity){
+        
+        try{
+            $conn->exec("set names 'utf8'");
+			$sql = "INSERT INTO `orders` (`productID`, `username`, `quantity`, `when`) VALUES ";
+            $sql.="( '$product', '$username', '$quantity', CURRENT_TIMESTAMP)";
+            $conn->exec($sql);
+        }
+        catch (Exception $e) {
+
+            return $e->getMessage();
+        }
+    }
+
+    public static function getPriceTable($conn, $c) {
+        try {
+            $conn->exec("set names 'utf8'");
+			$sql = "SELECT `id`,`description`,`price` FROM products WHERE `category` = ";
+            $sql .= $c;
+
+            $records = array();
+            foreach ($conn->query($sql) as $row) {
+                array_push($records, $row);
+            }
+            return json_encode($records);
+        } catch (Exception $e) {
+
+            return null;
+        }
+    }
+
+  
+
+}
